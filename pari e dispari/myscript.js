b61abb750a84722fa08fbc1f9bb90110cd91d664
AlessioAbbati <alex_117@live.it>
2023-04-20 19:35:34 +0200

    add prompt for evn or odd and number

---
+++ pari e dispari/myscript.js
@@ -0,0 +1,7 @@
+// faccio scegliere all'utente pari o dispari
+
+let choice = prompt("pari o dispari?");
+
+// dico all'utente di inserire un numero da 1 a 5
+
+let numUser = parseInt(prompt("scegli un numero da 1 a 5"));
