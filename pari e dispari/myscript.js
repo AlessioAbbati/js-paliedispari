b4a3a79a44412d544ff960362ce551d5cd03fc1a
AlessioAbbati <alex_117@live.it>
2023-04-20 20:07:06 +0200

    mod style to choice and add sum

--- pari e dispari/myscript.js
+++ pari e dispari/myscript.js
@@ -1,16 +1,20 @@
 // faccio scegliere all'utente pari o dispari
 
 let choice = prompt("pari o dispari?");
-console.log(choice);
+console.log("hai scelto:" + choice);
 
 // dico all'utente di inserire un numero da 1 a 5
 
 let numUser = parseInt(prompt("scegli un numero da 1 a 5"));
-console.log(numUser);
-
+console.log("hai scelto:" + numUser)
 // genero un numero random per il computer
+
 
 function generaNumeroCasuale(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  console.log(generaNumeroCasuale(1, 5));
+    
+}
+console.log("Bishop ha scelto:" + generaNumeroCasuale(1, 5));
+
+let sum = choice + generaNumeroCasuale;
+
