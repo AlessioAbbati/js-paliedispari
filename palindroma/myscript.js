1a9318eec10551b84518ce1fecd4329d58b08a74
AlessioAbbati <alex_117@live.it>
2023-04-20 18:06:26 +0200

    add function to compare the words

--- palindroma/myscript.js
+++ palindroma/myscript.js
@@ -2,4 +2,20 @@
 
 let word = prompt("inserisci una parola").toLowerCase();
 
-console.log(word);
+// creo la funzione per stabilire se la parola scelta è palindroma
+
+function palWord(word) {
+    let lunghezza = word.length;
+    for (let i = 0; i < Math.floor(lunghezza / 2); i++) {
+        if (word[i] !== word[lunghezza - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+if (palWord(word)) {
+    console.log("la parola è palindroma")
+} else {
+    console.log("la parola non è palindroma")
+}
