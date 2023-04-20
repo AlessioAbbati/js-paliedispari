c944c03faa332e71b7e84a1ffc9a9dd9065ab4c2
AlessioAbbati <alex_117@live.it>
2023-04-20 19:36:41 +0200

    add console log

--- pari e dispari/myscript.js
+++ pari e dispari/myscript.js
@@ -1,7 +1,9 @@
 // faccio scegliere all'utente pari o dispari
 
 let choice = prompt("pari o dispari?");
+console.log(choice);
 
 // dico all'utente di inserire un numero da 1 a 5
 
 let numUser = parseInt(prompt("scegli un numero da 1 a 5"));
+console.log(numUser);
